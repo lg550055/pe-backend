e01a3e67731cba08481371d0b3c63c78dd77f33d
lg550055 <lg550055@hotmail.com>
2022-06-13 20:55:45 -0500

    Sets production settings

--- pe_api_project/settings.py
+++ pe_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # TODO here ------------------------x
 ALLOWED_HOSTS = ['127.0.0.1', 'course-lib.herokuapp.com']
 
