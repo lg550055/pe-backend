bcf3da73050fdf7530d12df4cbf8a3dac21f3f62
lg550055 <lg550055@hotmail.com>
2022-07-02 23:04:05 -0500

    Adds get or create to stock route

--- pe/views.py
+++ pe/views.py
@@ -3,7 +3,7 @@ from .models import Estimate
 from .serializers import EstimateSerializer
 
 
-class EstimateList(generics.ListCreateAPIView):
+class EstimateList(generics.ListAPIView):
   queryset = Estimate.objects.all()
   serializer_class = EstimateSerializer
 
@@ -11,18 +11,19 @@ class EstimateDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Estimate.objects.all()
   serializer_class = EstimateSerializer
 
-class EstimateStock(generics.ListAPIView):
+class EstimateStock(generics.ListCreateAPIView):
   serializer_class = EstimateSerializer
   
   def get_queryset(self):
-    queryset = Estimate.objects.all()
+    """ if stock in db, return estimates, otherwise get estimates, create entry and return estimates"""
     ticker = self.request.query_params.get('ticker')
-    if ticker is not None:
-      queryset = queryset.filter(symbol=ticker)
+    if ticker:
+      queryset = Estimate.objects.filter(symbol=ticker)
       if not queryset:
+        # get estimates and create db entry
         locObj = {
             # symbol: ticker, num_analysts: 0, fwd_eps: 0, fwd2_eps: 0, fwd_rev: 0, fwd2_rev: 0, fwd_rev_g: 0, fwd2_rev_g: 0
         }
         Estimate.objects.create(symbol=ticker, num_analysts=0, fwd_eps=0, fwd2_eps=0, fwd_rev=0, fwd2_rev=0, fwd_rev_g=0, fwd2_rev_g=0)
-        queryset = queryset.filter(symbol=ticker)
+        queryset = [Estimate.objects.get(symbol=ticker)]
     return queryset
