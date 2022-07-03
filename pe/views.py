564472b6ac4601ab01a491dcd7587560e6310a05
lg550055 <lg550055@hotmail.com>
2022-07-03 09:36:34 -0500

    Adds gets actual estimates for new stock

--- pe/views.py
+++ pe/views.py
@@ -1,6 +1,9 @@
 from rest_framework import generics
 from .models import Estimate
 from .serializers import EstimateSerializer
+import requests
+import json
+import re
 
 
 class EstimateList(generics.ListAPIView):
@@ -21,9 +24,22 @@ class EstimateStock(generics.ListCreateAPIView):
       queryset = Estimate.objects.filter(symbol=ticker)
       if not queryset:
         # get estimates and create db entry
-        locObj = {
-            # symbol: ticker, num_analysts: 0, fwd_eps: 0, fwd2_eps: 0, fwd_rev: 0, fwd2_rev: 0, fwd_rev_g: 0, fwd2_rev_g: 0
-        }
-        Estimate.objects.create(symbol=ticker, num_analysts=0, fwd_eps=0, fwd2_eps=0, fwd_rev=0, fwd2_rev=0, fwd_rev_g=0, fwd2_rev_g=0)
+        headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) (KHTML, like Gecko) Chrome/102.0.5005.63'}
+        url = 'https://finance.yahoo.com/quote/'+ ticker + '/analysis?p=' + ticker
+        page = requests.get(url, headers=headers, timeout=5)
+        data = json.loads(re.search('root\.App\.main\s*=\s*(.*);', page.text).group(1))
+        f = [t for t in data["context"]["dispatcher"]["stores"]["QuoteSummaryStore"]["earningsTrend"]["trend"] if t["period"] in ['0y','+1y']]
+        num_analysts = f[0]['earningsEstimate']['numberOfAnalysts']['fmt']
+        fwd_eps = f[0]['earningsEstimate']['avg']['fmt']
+        fwd_rev = f[0]['revenueEstimate']['avg']['raw'] / 1e9
+        fwd_rev_g = f[0]['revenueEstimate']['growth']['raw']
+        fwd2_eps = f[1]['earningsEstimate']['avg']['fmt']
+        fwd2_rev = f[1]['revenueEstimate']['avg']['raw'] / 1e9
+        fwd2_rev_g = f[1]['revenueEstimate']['growth']['raw']
+
+        Estimate.objects.create(symbol=ticker, num_analysts=num_analysts, 
+          fwd_eps=fwd_eps, fwd2_eps=fwd2_eps, fwd_rev=fwd_rev, fwd2_rev=fwd2_rev, 
+          fwd_rev_g=fwd_rev_g, fwd2_rev_g=fwd2_rev_g)
         queryset = [Estimate.objects.get(symbol=ticker)]
+
     return queryset
